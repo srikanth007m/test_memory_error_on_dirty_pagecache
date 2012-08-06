04e508d9b79f3e5427ad1b608d85699746c37f16
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2012-08-06 11:57:25 -0400

    Refactor test_multifiles
    Signed-off-by: Naoya Horiguchi <nao.horiguchi@gmail.com>

--- do_test_multifiles.sh
+++ do_test_multifiles.sh
@@ -9,24 +9,21 @@ for i in `seq 1 1000` ; do
     for type in read writefull ; do
         testf=${filebase}${type}${i}
         ruby -e 'puts "0"*8192' > $testf
-        ./test ${testf} 2 ${type} onerror > /dev/null
+        ./test ${testf} 2 ${type} onerror > /dev/null 2>&1
     done
 done
 
-for i in `seq 1 1000` ; do
-    for type in read writefull ; do
-        testf=${filebase}${type}${i}
-        cat ${testf} > /dev/null 2>&1
-        echo $?
-        rm ${testf}
-    done
-done
+corrupted3=`grep -i corrupt /proc/meminfo | tr -s ' ' | cut -f2 -d' '`
+
+rm ${filebase}*
+
+corrupted4=`grep -i corrupt /proc/meminfo | tr -s ' ' | cut -f2 -d' '`
 
 page-types -b hwpoison -x
 
 corrupted2=`grep -i corrupt /proc/meminfo | tr -s ' ' | cut -f2 -d' '`
 if [ ! "$corrupted1" = "$corrupted2" ] ; then
-    echo "FAIL: \"HardwareCorrupted:\" does not match between before/after testing ($corrupted1, $corrupted2)"
+    echo "FAIL: \"HardwareCorrupted:\" does not match between before/after testing ($corrupted1, $corrupted3 $corrupted4 $corrupted2)"
     fail=$[fail + 1]
 fi
 
